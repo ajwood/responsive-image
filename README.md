a1a7ae5ddfa435b53d72655bf31eeb286998fb9a
Andrew Wood <wooderton2@gmail.com>
2021-12-30 10:58:36 -0500

    Switch flask/html implementation to pure js
    This is simpler (doesn't need an api) and behaves better. The way htmx
    replaced the whole `<img>` element resulted in flicker. This way, the
    element is not removed and replaced, only its src updated, resulting in
    smoother transitions.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Playing with htmx
+Playing with blender render as a responsive web asset.
+
+https://blender.stackexchange.com/questions/244763/turning-interactive-geometry-nodes-into-interactive-web-asset#comment422203_244763
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
+# constants based on how the images were rendered (the image dimension, and how
+# many grid positions exist)
 WIDTH_PX = 432
 HEIGHT_PX = 432
 HEIGHT_N = 11
@@ -13,28 +15,14 @@ WIDTH_N = 11
 def index(img="0000"):
     return render_template(
         "base.html",
-        img0=make_img(img, id="baseimage", usemap=None),
-        img1=make_img(img),
+        img=img,
         w=WIDTH_PX,
         h=HEIGHT_PX,
         wn=WIDTH_N,
         hn=HEIGHT_N,
     )
 
-
-def make_img(img, id="mycanvas", usemap="#imgmap"):
-    usemap = "" if usemap is None else f'usemap="{usemap}"'
-    elt = f'<img style="position:absolute; top:0; left:0;" src="images/{img}" width="{WIDTH_PX}" height="{HEIGHT_PX}" {usemap} />'
-    return elt
-
-
-@app.route("/hx/imgmap")
-def hx_imgmap():
-    img = request.args["img"]
-    return make_img(img)
-
-
-@app.route("/images/<n>")
-def images(n):
+@app.route("/images/<img>")
+def images(img):
     """Static image server"""
-    return send_file(os.path.join("images.jpg", f"{n}.jpg"), mimetype="image/png")
+    return send_file(os.path.join("images.jpg", f"{img}.jpg"), mimetype="image/png")
